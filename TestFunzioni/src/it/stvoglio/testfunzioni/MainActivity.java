55c16f8d459c7e71bf2b21b8508f0a95ce7670ff
Stefano Vogliotti #1 <stvoglio@alice.it>
2014-02-10 16:27:06 +0100

    201402101626 : non funzionante

--- TestFunzioni/src/it/stvoglio/testfunzioni/MainActivity.java
+++ TestFunzioni/src/it/stvoglio/testfunzioni/MainActivity.java
@@ -11,53 +11,48 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_main);
-			
-		Button bottoneInviaMailSenzaInterazione = (Button)findViewById(R.id.bottoneInviaMailSenzaInterazione);
-		bottoneInviaMailSenzaInterazione.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				// Mail m = new Mail("gmailusername@gmail.com", "password");
-				
-			}
-		});
-		
-		/*
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		
-		  bottoneInviaMailSenzaInterazione.setOnClickListener(new View.OnClickListener() { 
-		    public void onClick(View view) { 
-		      Mail m = new Mail("gmailusername@gmail.com", "password"); 
-		 
-		      String[] toArr = {"bla@bla.com", "lala@lala.com"}; 
-		      m.setTo(toArr); 
-		      m.setFrom("wooo@wooo.com"); 
-		      m.setSubject("This is an email sent using my Mail JavaMail wrapper from an Android device."); 
-		      m.setBody("Email body."); 
-		 
-		      try { 
-		        m.addAttachment("/sdcard/filelocation"); 
-		 
-		        if(m.send()) { 
-		          Toast.makeText(MailApp.this, "Email was sent successfully.", Toast.LENGTH_LONG).show(); 
-		        } else { 
-		          Toast.makeText(MailApp.this, "Email was not sent.", Toast.LENGTH_LONG).show(); 
-		        } 
-		      } catch(Exception e) { 
-		        //Toast.makeText(MailApp.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show(); 
-		        Log.e("MailApp", "Could not send email", e); 
-		      } 
-		    } 
-		  }); 
-*/
-		
+
+		Button bottoneInviaMailSenzaInterazione = (Button) findViewById(R.id.bottoneInviaMailSenzaInterazione);
+		bottoneInviaMailSenzaInterazione
+				.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						// TODO Auto-generated method stub
+						Mail m = new Mail("stvoglio@gmail.com", "ZK01SKaK");
+						String[] toArr = { "stvoglio@tiscali.it",
+								"stvoglio@gmail.com" };
+						m.setSubject("This is an email sent using my Mail JavaMail wrapper from an Android device.");
+						try {
+							m.setBody("Email body.");
+
+						} catch (Exception e) {
+							//
+						}
+						/*
+						 * 
+						 * 
+						 *                    try { 
+						 *         m.addAttachment("/sdcard/filelocation");   
+						 *         if(m.send()) { 
+						 *           Toast.makeText(MailApp
+						 * .this, "Email was sent successfully."
+						 * , Toast.LENGTH_LONG).show();          } 
+						 * 
+						 * else {            Toast.makeText
+						 * (MailApp.this, "Email was not sent."
+						 * , Toast.LENGTH_LONG).show();          } 
+						 *       } catch(Exception e) { 
+						 *         //Toast.makeText(MailApp
+						 * .this, "There was a problem sending the email."
+						 * , Toast.LENGTH_LONG).show(); 
+						 *         Log.e("MailApp", "Could not send email", e); 
+						 *       }      }    }); 
+						 */
+					}
+				});
+
 	}
 
 	@Override
