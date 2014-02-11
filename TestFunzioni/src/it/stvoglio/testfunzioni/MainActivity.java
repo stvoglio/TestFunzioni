698d6679e5070c7e6529b0c91bfe88958736c1cd
Stefano Vogliotti #1 <stvoglio@alice.it>
2014-02-11 11:24:27 +0100

    201402111124 : compila ma non invia

--- TestFunzioni/src/it/stvoglio/testfunzioni/MainActivity.java
+++ TestFunzioni/src/it/stvoglio/testfunzioni/MainActivity.java
@@ -2,9 +2,11 @@ package it.stvoglio.testfunzioni;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -17,43 +19,36 @@ public class MainActivity extends Activity {
 		Button bottoneInviaMailSenzaInterazione = (Button) findViewById(R.id.bottoneInviaMailSenzaInterazione);
 		bottoneInviaMailSenzaInterazione
 				.setOnClickListener(new View.OnClickListener() {
+
 					@Override
-					public void onClick(View view) {
-						// TODO Auto-generated method stub
-						Mail m = new Mail("stvoglio@gmail.com", "ZK01SKaK");
+					public void onClick(View arg0) {
+						MyMail m = new MyMail("stvoglio@gmail.com", "ZK01SKaK");
+						m.setFrom("stvoglio@gmail.com");
 						String[] toArr = { "stvoglio@tiscali.it",
 								"stvoglio@gmail.com" };
+						m.setTo(toArr);
 						m.setSubject("This is an email sent using my Mail JavaMail wrapper from an Android device.");
-						try {
-							m.setBody("Email body.");
+						m.setBody("Email body.");
 
+						try {
+							if (m.send()) {
+								Toast.makeText(MainActivity.this,
+										"Email was sent successfully.",
+										Toast.LENGTH_LONG).show();
+							} else {
+								Toast.makeText(MainActivity.this,
+										"Email was not sent.",
+										Toast.LENGTH_LONG).show();
+							}
 						} catch (Exception e) {
-							//
+							Toast.makeText(MainActivity.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show();
+							Log.e("MailApp", "Could not send email", e);
 						}
-						/*
-						 * 
-						 * 
-						 *                    try { 
-						 *         m.addAttachment("/sdcard/filelocation");   
-						 *         if(m.send()) { 
-						 *           Toast.makeText(MailApp
-						 * .this, "Email was sent successfully."
-						 * , Toast.LENGTH_LONG).show();          } 
-						 * 
-						 * else {            Toast.makeText
-						 * (MailApp.this, "Email was not sent."
-						 * , Toast.LENGTH_LONG).show();          } 
-						 *       } catch(Exception e) { 
-						 *         //Toast.makeText(MailApp
-						 * .this, "There was a problem sending the email."
-						 * , Toast.LENGTH_LONG).show(); 
-						 *         Log.e("MailApp", "Could not send email", e); 
-						 *       }      }    }); 
-						 */
+
 					}
 				});
 
-	}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
